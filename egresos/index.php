5cca4ba98dbcc59f4b918b9d6fb83f7ce82d1d3c
coderoycc <rcchambi4@gmail.com>
2023-11-10 19:41:33 -0400

    inicio

---
+++ egresos/index.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+  <meta charset="utf-8" />
+  <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+  <title>INGRESOS</title>
+  <link rel="stylesheet" href="../assets/datatables/datatables.bootstrap5.min.css">
+  <link href="../css/styles.css" rel="stylesheet" />
+  <script src="../assets/fontawesome/fontawesome6.min.js"></script>
+  <script src="../assets/jquery/jquery.js"></script>
+</head>
+
+<body>
+  <?php include("../common/header.php"); ?>
+  <div id="layoutSidenav"> <!-- contenedor -->
+    <?php include("../common/sidebar.php"); ?>
+    <div id="layoutSidenav_content">
+      <main>
+        <div class="container-fluid px-4">
+          <h1 class="mt-4">Ingresos</h1>
+          <ol class="breadcrumb mb-4">
+            <li class="breadcrumb-item active">Lista Ingresos</li>
+          </ol>
+        </div>
+        <!-- <?php //include('../common/footer.php'); 
+              ?> -->
+      </main>
+    </div>
+  </div><!-- fin contenedor -->
+
+  <script src="../assets/bootstrap/js/bootstrap.bundle.min.js"></script>
+  <script src="../js/scripts.js"></script>
+  <script src="../assets/datatables/datatables.jquery.min.js"></script>
+  <script src="../assets/datatables/datatables.bootstrap5.min.js"></script>
+</body>
+
+</html>
